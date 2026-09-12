f754a6faeffd74d8b5b216abaa2a11ddceb45156
taoualikamal-spec <274307107+taoualikamal-spec@users.noreply.github.com>
2026-09-13 00:39:50 +0100

    Simplify phone-first interface

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kamal-control-center-v1';
+const CACHE_NAME = 'kamal-control-center-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
